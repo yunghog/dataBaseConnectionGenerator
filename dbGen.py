8b6489e3890f1f192475a86f8992980227e83c50
Samartha <41548444+yunghog@users.noreply.github.com>
2020-04-08 11:28:43 +0530

    main

--- dbGen.py
+++ dbGen.py
@@ -1,8 +1,24 @@
-dbCon=open('dbConnect.php','w')
+#dbGen.py : Easy way to generate Database connection file for your project
+#author : Samartha
+
+format = input('Input extension of DBConnect file (js/php/java) : ')
+platform = input('Input Database provider (mysql/oracle) :  ')
 dbServer = input('Enter the Database Server URL : ')
 dbName = input('Enter the Database Name : ')
 dbUser = input('Enter the Username : ')
-dbPser = input('Enter the Database Password : ')
-dbCon.write(dbServer)
-dbCon.write(dbServer)
-dbCon.write(dbServer)
+dbPswd = input('Enter the Database Password : ')
+if format=='js':
+    dbCon = open(r'dbConnect.js','w')
+    dbCon.write("var mysql = require('mysql')\n")
+    dbCon.write("var con = mysql.createConnection({\n")
+    host="host: " + dbServer + ",\n"
+    dbCon.write(host)
+    dbUser="user: " + dbName + ",\n"
+    dbCon.write(dbUser)
+    dbPswd="passsword: " + dbPswd + ",\n"
+    dbCon.write(dbPswd)
+    dbCon.write("})")
+    dbCon.write("con.connect(function(err)){\n")
+    dbCon.write("if(err) throw err;\n")
+    dbCon.write("console.log('Connected')\n")
+    dbCon.write("});")
