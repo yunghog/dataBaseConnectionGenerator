416288362690e5b671e9c6368c1689a860c7575b
Samartha <41548444+yunghog@users.noreply.github.com>
2020-04-17 18:29:48 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#Database Connection Generator
+#author : Samartha
+import os
+os.system('cls')
+banner="""
+    ██████╗ ██████╗        ██████╗ ██████╗ ███╗   ██╗███╗   ██╗███████╗ ██████╗████████╗
+    ██╔══██╗██╔══██╗      ██╔════╝██╔═══██╗████╗  ██║████╗  ██║██╔════╝██╔════╝╚══██╔══╝
+    ██║  ██║██████╔╝█████╗██║     ██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██║        ██║
+    ██║  ██║██╔══██╗╚════╝██║     ██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██║        ██║
+    ██████╔╝██████╔╝      ╚██████╗╚██████╔╝██║ ╚████║██║ ╚████║███████╗╚██████╗   ██║
+    ╚═════╝ ╚═════╝        ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═╝
+    ----------create database connection for your project without any effort----------
+                                -----by samartha-----
+"""
+print(banner)
+print(':::::DB-Connect:::::')
+print('dbGenCli.py - Generate DB Connection via CLI')
+print('dbGenGui.py - Generate DB Connection via GUI')
